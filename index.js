0674b93397a92613a9cd3d0935bf88c5e0ead508
Qu33nArlene <ahaskins93@yahoo.com>
2022-06-17 16:23:29 -0400

    added menu function and options

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const express = require('express');
 const inquirer = require('inquirer');
 const db = require('./db/connection');
 require('console.table');
@@ -13,20 +12,26 @@ function menu() {
             choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Quit']
         }
     ])
-    .then ((results) => {
-        let option = results.options;
-        if (option === 'View all departments') {
-            viewDepartments();
-        } else if (option === 'View all roles') {
-            viewRoles();
-        }
-        //etc etc etc
-        else if (option === 'Quit') {
-            quitMenu();
-        }
-    })
-
-
+        .then((results) => {
+            let option = results.options;
+            if (option === 'View all departments') {
+                viewDepartments();
+            } else if (option === 'View all roles') {
+                viewRoles();
+            } else if (option === 'View all employees') {
+                viewEmployees();
+            } else if (option === 'Add a department') {
+                addDepartment();
+            } else if (option === 'Add a role') {
+                addRole();
+            } else if (option === 'Add an employee') {
+                addEmployee();
+            } else if (option === 'Update an employee role') {
+                updateRole();
+            } else {
+                quitMenu();
+            }
+        })
 }
 
 function viewDepartments() {
@@ -37,7 +42,58 @@ function viewDepartments() {
     })
 }
 
+function viewRoles() {
+    db.query('SELECT * FROM role', (err, result) => {
+        if (err) throw err
+        console.table(result);
+        menu();
+    })
+}
+
+function viewEmployees() {
+    db.query('SELECT * FROM employee', (err, result) => {
+        if (err) throw err
+        console.table(result);
+        menu();
+    })
+}
+
+function addDepartment() {
+    db.query('INSERT INTO department WHERE id=?', (err, result) => {
+        if (err) throw err
+        console.table(result);
+        menu();
+    })
+}
+
+function addRole() {
+    db.query('INSERT INTO role WHERE id=?', (err, result) => {
+        if (err) throw err
+        console.table(result);
+        menu();
+    })
+}
+
+function addEmployee() {
+    db.query('INSERT INTO employee WHERE id=?', (err, result) => {
+        if (err) throw err
+        console.table(result);
+        menu();
+    })
+
+}
+
+function updateRole() {
+    db.query('UPDATE role WHERE id=?', (err, result) => {
+        if (err) throw err
+        console.table(result);
+        menu();
+    })
+}
+
 function quitMenu() {
-    console.log('Goodbye');
+    console.log("Thank you for using Arlene's Employee Tracker. Goodbye!");
     process.exit();
 }
+
+menu();
